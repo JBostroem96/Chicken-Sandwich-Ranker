8adaf39f4a343c579e68063e79ef6e9e7fd6d9b8
JBostroem96 <Jbostroem20@outlook.com>
2024-07-15 09:37:18 -0500

    Removed leftover code

--- UserChickenSandwich.php
+++ UserChickenSandwich.php
@@ -110,34 +110,6 @@
                         return $this;
             }
 
-            //This function's purpose is to display the chicken sandwich
-            public function display($chicken_sandwich) {
-                    
-                echo "<div class='score'>" 
-                    . "<div class='styling'><form action='user_chicken_sandwich_service.php' method='POST'>
-                    <p class='text-white fw-bold'>Your rating for {$chicken_sandwich->getName()}:"; 
-                        
-                if (isset($_POST['edit_entry'])) {
-                                
-                    if ($_POST['chicken_id'] == $chicken_sandwich->getId()) {
-                                    
-                        echo "<input type='numeric' name='score' pattern='[1-9]|10'>";
-                        echo "<button class='button' type='submit' id='edit_score'
-                            name='edit_score' value='{$_POST['chicken_id']}'>RATE ME!</button><p>Only 1-10 is allowed, one rating per account</p>";
-                    }
-                                
-                } else {
-
-                    echo "{$chicken_sandwich->getScore()}<br>";
-
-                    echo "<input type='hidden' name='chicken_id' value='{$chicken_sandwich->getChicken_id()}'>
-                        <button class='button' type='submit' id='delete_entry'
-                        name='delete_entry' value='{$chicken_sandwich->getScore()}'>DELETE</button>"
-                        . "<button class='button' type='submit' id='edit_entry'
-                        name='edit_entry'>EDIT</button></form></div></div>";
-                }   
-            }
-
         }
                 
 ?>
